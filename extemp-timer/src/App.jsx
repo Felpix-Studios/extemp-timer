e4ce2957d5a1a3cf84f50100367a97b655f1484a
Kevin Li <jshiansongli@gmail.com>
2023-03-29 17:51:32 -0400

    changes

--- extemp-timer/src/App.jsx
+++ extemp-timer/src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function Title() {
+  return (
+    <h1 className="text-center text-3xl font-bold">Extemp Timer</h1>
+  )
+}
+
+function CurrentTime() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  setInterval(() => {
+    setTime(new Date().toLocaleTimeString())
+  }, 1000)
+  return (
+    <p className="text-center text-xl font-bold">{time}</p>
+  )
+}
+
+function Speaker( {speaker} ) {
+  
+  return (
+    <div >
+      <h2 className="text-center text-2xl font-bold">Name: </h2>
+      <p className="text-left text-xl font-bold">Draw Time: </p>
+      <p className="text-left text-xl font-bold">Speak Time: </p>
+    </div>
+  )
+}
+
+function SpeakerList({speakers}) {
+  return (
+		<div className="grid grid-cols-3 gap-4">
+      {Array.from(Array(speakers).keys()).map((speaker) => (
+        <Speaker key={speaker} speaker={speaker} />
+      ))}
+		</div>
+  );
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [speakers, setSpeakers] = useState(6)
+  const [interval, setInterval] = useState(10)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container mx-auto">
+      <Title />
+      <CurrentTime />
+      <SpeakerList speakers={speakers}/>
     </div>
   )
 }
