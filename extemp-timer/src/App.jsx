c47f28205b2137dec678405e76fe65f61f7176e9
Felpix <jshiansongli@gmail.com>
2023-04-01 23:22:29 -0400

    time input

--- extemp-timer/src/App.jsx
+++ extemp-timer/src/App.jsx
@@ -39,6 +39,7 @@ function SpeakerList({speakers}) {
 }
 
 function RoundConfig({interval, speaker, setInterval, setSpeakers, roundStartTime, setRoundStartTime}) {
+
   return (
 		<div className="grid grid-cols-3 gap-24 m-4 text-center px-24">
 			<div>
@@ -52,7 +53,7 @@ function RoundConfig({interval, speaker, setInterval, setSpeakers, roundStartTim
 			</div>
 			<div>
 				<label>First Draw Time</label>
-        <p>{roundStartTime}</p>
+        <input type="time" className="mt-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" onChange={(e)=>console.log(e.target.value)}></input>
 			</div>
 			<div>
 				<label>Interval (Minute)</label>
